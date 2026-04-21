541953da1e87f9840ae1bdaa28b6763de46180a2
clauds <waguramarie@gmail.com>
2026-04-21 13:54:32 +0200

    machine agent class

---
+++ src/manufacturing/MachineAgent.java
@@ -0,0 +1,27 @@
+package manufacturing;
+
+import jade.core.Agent;
+
+public class MachineAgent extends Agent {
+    // Class variable to store the machine's capability
+    private String capability;
+
+    @Override
+    protected void setup() {
+        // Read the arguments passed to the agent
+        Object[] args = getArguments();
+
+        if (args != null && args.length > 0) {
+            capability = (String) args[0];
+            System.out.println("Machine Agent " + getAID().getName() + " is ready.");
+            System.out.println("Registered capability: " + capability);
+        } else {
+            System.out.println("WARNING: Machine Agent " + getAID().getName() + " started with NO capability argument!");
+        }
+    }
+
+    @Override
+    protected void takeDown() {
+        System.out.println("Machine Agent " + getAID().getName() + " terminating.");
+    }
+}
